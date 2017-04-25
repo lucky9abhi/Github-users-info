f4b7f9b91455cafefd6fe502cae99ee5ea77c0e3
Abhi Abz <abhiui18@gmail.com>
2017-04-25 17:45:07 -0500

    Weather app Update 4/25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { SupportComponent } from './support/support.component';
+import {GithubService} from "./services/github.service";
 
+
+const appRoutes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'circular', component:CircularComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'feedback', component:FeedbackComponent},
+  {path: 'support', component:SupportComponent}
+  ];
 
 @NgModule({
   declarations: [
@@ -15,13 +28,17 @@ import { HomeComponent } from './home/home.component';
     CircularComponent,
     NavbarComponent,
     HomeComponent,
+    ContactComponent,
+    FeedbackComponent,
+    SupportComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [GithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/circular/circular.component.ts
+++ src/app/circular/circular.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from "../services/github.service";
 
 @Component({
   selector: 'app-circular',
   templateUrl: './circular.component.html',
-  styleUrls: ['./circular.component.css']
+  styleUrls: ['./circular.component.css'],
+  providers:[GithubService]
 })
 export class CircularComponent implements OnInit {
 
-  public happyText = 'Hopefully god makes me happy next month !!! ';
+  public searchText ;
 
-  constructor() {
+  constructor(private githubService: GithubService) {
   }
 
   ngOnInit() {
+  }
 
+  onKeyup(event){
+    this.searchText = event.target.value;
+  }
+
+  getUsers(){
+    this.githubService.getUser(this.searchText).subscribe(
+      res => {
+        console.log(res);
+      }
+    );
   }
 }
