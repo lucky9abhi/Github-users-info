2f87332b868b8c43bfac18c91f89504c2f769022
Abhi Abz <abhiui18@gmail.com>
2017-04-24 19:14:38 -0500

    Weather app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CircularComponent } from './circular/circular.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircularComponent,
+    NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/circular/circular.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circular',
+  templateUrl: './circular.component.html',
+  styleUrls: ['./circular.component.css']
+})
+export class CircularComponent implements OnInit {
+
+  public happyText = 'Hopefully god makes me happy next month !!! ';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+}
